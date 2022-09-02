e86c183263dfeb71030c93790770fce6a6832d51
Iryna Movchan <movchaniryna@ukr.net>
2022-09-02 22:33:06 +0300

    Добавляет задание 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const photoGallery = {
+  gallery: document.querySelector(".gallery"),
+};
+
+createGalleryItems();
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+function createGalleryItems() {
+  const galleryMarkup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li>
+    <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a></li>`;
+    })
+    .join("");
+  photoGallery.gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
+}
