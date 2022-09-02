81abf26d83cdc19e81d7d82360b5d5cf2ace9d24
Iryna Movchan <movchaniryna@ukr.net>
2022-09-02 22:17:57 +0300

    Добавляет задание 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,63 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const photoGallery = {
+  gallery: document.querySelector(".gallery"),
+};
 
-console.log(galleryItems);
+createGalleryItems();
+
+photoGallery.gallery.addEventListener("click", onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const originalPhotoSrc = event.target.dataset.source;
+  openModal(originalPhotoSrc);
+}
+
+function createGalleryItems() {
+  const galleryMarkup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+  photoGallery.gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
+}
+
+function openModal(sourse) {
+  const instance = basicLightbox.create(
+    `
+    <img src="${sourse}" width="800" height="600">
+`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", onKeyDownEscape);
+        console.log("Show");
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", onKeyDownEscape);
+        console.log("Close");
+      },
+    }
+  );
+  instance.show();
+
+  function onKeyDownEscape(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  }
+}
